aa04b0d48bf6fe42b6a63d4ca7fe5f74090d7cff
TUĞÇE AKSOY <70168455+tugceaksy@users.noreply.github.com>
2021-11-17 14:46:00 +0300

    Add files via upload

---
+++ svg-turkiye-haritasi.js
@@ -0,0 +1,86 @@
+/*! SVG Türkiye Haritası | MIT Lisans | dnomak.com */
+
+function svgturkiyeharitasi() {
+  const element = document.querySelector('#svg-turkiye-haritasi');
+  const info = document.querySelector('.il-isimleri');
+
+  element.addEventListener(
+    'mouseover',
+    function (event) {
+      if (event.target.tagName === 'path' && event.target.parentNode.id !== 'guney-kibris') {
+        info.innerHTML = [
+          '<div>',
+          event.target.parentNode.getAttribute('data-iladi'),
+          '</div>'
+        ].join('');
+      }
+    }
+  );
+
+  element.addEventListener(
+    'mousemove',
+    function (event) {
+      info.style.top = event.pageY + 25 + 'px';
+      info.style.left = event.pageX + 'px';
+    }
+  );
+
+  element.addEventListener(
+    'mouseout',
+    function (event) {
+      info.innerHTML = '';
+    }
+  );
+
+  element.addEventListener(
+    'click',
+  
+   
+    function (event) {
+      if (event.target.tagName === 'path') {
+        const parent = event.target.parentNode;
+        const id = parent.getAttribute('id');
+        var modal = document.getElementById("myModal");
+    
+  
+        // Get the <span> element that closes the modal
+        var span = document.getElementsByClassName("close")[0];
+        
+        // When the user clicks on the button, open the modal 
+        element.onclick = function() {
+          modal.style.display = "block";
+        }
+   
+        document.getElementById('secilen').innerHTML = "SEÇİLEN İL:"+[
+        
+          event.target.parentNode.getAttribute('data-iladi')
+         
+        ];
+        // When the user clicks on <span> (x), close the modal
+        span.onclick = function() {
+          modal.style.display = "none";
+        }
+        
+        // When the user clicks anywhere outside of the modal, close it
+        window.onclick = function(event) {
+          if (event.target == modal) {
+            modal.style.display = "none";
+          }
+        }
+ 
+        if (
+          id === 'guney-kibris'
+        ) {
+          return;
+        }
+
+        window.location.href = (
+          '#'
+          + id
+          + '-'
+          + parent.getAttribute('data-plakakodu')
+        );
+      }
+    }
+  );
+}
